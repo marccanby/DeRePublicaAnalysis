49d3d94de44e836dfd0265b85c994472b353a089
marccanby <marc.canby@gmail.com>
2018-05-02 19:04:50 +0000

    update

--- nonparam.py
+++
@@ -1,6 +0,0 @@
-from utils import get_text_by_author
-from utils import cleanup_sent
-
-cicero_texts_dirty = get_text_by_author('cicero/', verbose = True, flatten = True)
-cicero_texts = {x: list(map(cleanup_sent, cicero_texts_dirty[x])) for x in cicero_texts_dirty}
-
